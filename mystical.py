440cb8326ff5e1b7e3bc4e113ed7bcf85e73f6ee
jit-a11y-cmyk <businkavtakom@gmail.com>
2026-05-08 20:19:20 +0300

    Update mystical.py

--- mystical.py
+++ mystical.py
@@ -26,7 +26,7 @@ PUBLISH_INTERVAL = 180
 # ======================= LLaMA / GROQ =======================
 
 client = OpenAI(
-    api_key= os.getenv("GROQ_API_KEY")
+    api_key=os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1"
 )
 
